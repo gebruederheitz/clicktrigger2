75fa2235ea1971ac8b4b4a9713e426d5c3bf2e6b
AndreasMaros <andreas.maros@gebruederheitz.de>
2022-09-30 13:19:07 +0200

    [chore] Fix demos

--- .rollup.config.js
+++ .rollup.config.js
@@ -149,6 +149,7 @@ export default [
                     },
                 },
             }),
+            !production && serve(),
             !production && livereload({
                 watch: ['./demo/', './dist'],
             }),
